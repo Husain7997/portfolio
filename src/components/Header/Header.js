36303e67b0d01c5b9797ac453489f744aa1924ac
husainahmed97 <68569532+husainahmed97@users.noreply.github.com>
2023-01-27 05:06:18 +0600

    navbar and header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import header from '../../assets/header.jpg'
+
+const Header = () => {
+    return (
+        <header className="hero bg-base-200">
+        <div className="hero-content justify-between flex-col lg:flex-row-reverse">
+          <img src={header} alt='husain ahmed' className="max-w-sm  rounded-lg shadow-2xl" />
+          <div className='md:mx-5'>
+            <h1 className="text-5xl font-bold"><span className='text-2xl text-green-500'>Hi, my name is</span> <br/> Husain Ahmed <br/>Web Developer</h1>
+            <p className="py-6">I’m a software engineer specializing in building<br/>
+             (and occasionally designing) exceptional digital experiences.</p>
+            <button className="btn bg-green-500">Contact me!</button>
+          </div>
+        </div>
+      </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.jpeg'
+
+const Navbar = () => {
+    return (
+        <navbar className="navbar flex justify-between bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <li><Link to='/'>About</Link></li>
+            <li><Link to='/'>My Skills</Link></li>
+            <li><Link to='/'>Work</Link></li>
+            <li><Link to='/'>Contact</Link></li>
+            <li><Link to='/'>Blog</Link></li>
+            </ul>
+          </div>
+          <Link to='/'> className="btn btn-ghost normal-case text-xl">
+          <div className="w-1/4 rounded-full ring ring-offset-base-100 ring-offset-2">
+    <img src={logo} alt='husain ahmed' className='rounded' />
+  </div>
+        </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><Link to="/">About</Link></li>
+            <li><Link to="/">My Skills</Link></li>
+            <li><Link to="/">Work</Link></li>
+            <li><Link to="/">Contact</Link></li>
+            <li><Link to="/">Blog</Link></li>
+            {/* <li><a>Blog</a></li> */}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Resume</a>
+        </div>
+      </navbar>
+    );
+};
+
+export default Navbar;
